7c7ab02a0fe4160d8b6c6a1166ab20826f2bb433
zhangzhihe <zhangzhihe@nsfocus.com>
2017-07-25 19:09:34 +0800

    sql edit

--- doc/full.sql
+++ doc/full.sql
@@ -8,20 +8,21 @@ SET character_set_database = 'utf8';
 
 CREATE TABLE IF NOT EXISTS `aere`.`request_record` (
   `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键ID',
-  `global_id` VARCHAR(16) NOT NULL COMMENT '全局request_id',
-  `parnet_id` VARCHAR(16) NULL COMMENT '上级request_id',
+  `global_id` VARCHAR(32) NOT NULL COMMENT '全局request_id',
+  `parnet_id` VARCHAR(32) NULL COMMENT '上级request_id',
   `current_id` VARCHAR(16) NOT NULL COMMENT '自己request_id',
   `deep_num` INT(11) NOT NULL COMMENT '层级',
   `index_num` INT(11) NOT NULL COMMENT '顺序号',
   `http_status` INT(11) NOT NULL DEFAULT 200 COMMENT 'HTTP状态',
   `nscloud_status` INT(11) NOT NULL DEFAULT 2000 COMMENT '绿盟云状态',
   `method` VARCHAR(10) NOT NULL COMMENT '请求方式',
+  `path` VARCHAR(128) NOT NULL COMMENT '请求URL',
   `module` VARCHAR(45) NOT NULL COMMENT '服务名称',
   `remote_ip` VARCHAR(64) NOT NULL DEFAULT '127.0.0.1' COMMENT '请求IP',
   `request_data` LONGTEXT NOT NULL DEFAULT '' COMMENT '请求数据',
   `response_data` LONGTEXT NOT NULL DEFAULT '' COMMENT '返回数据',
-  `request_time` INT(11) NOT NULL COMMENT '请求时间',
-  `response_time` INT(11) NOT NULL COMMENT '返回时间',
+  `taking` INT(11) NOT NULL COMMENT '耗时（毫秒）',
+  `create_time` INT(11) NOT NULL COMMENT '创建（unix时间戳）',
   PRIMARY KEY (`id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
--- rs/ec/models.py
+++ rs/ec/models.py
@@ -5,20 +5,21 @@ from django.db import models
 
 
 class RequestRecord(models.Model):
-    global_id = models.CharField(max_length=16)
-    parnet_id = models.CharField(max_length=16, blank=True, null=True)
-    current_id = models.CharField(max_length=16)
+    global_id = models.CharField(max_length=32)
+    parnet_id = models.CharField(max_length=32, blank=True, null=True)
+    current_id = models.CharField(max_length=32)
     deep_num = models.IntegerField()
     index_num = models.IntegerField()
     http_status = models.IntegerField()
     nscloud_status = models.IntegerField()
     method = models.CharField(max_length=10)
+    path = models.CharField(max_length=128)
     module = models.CharField(max_length=45)
     remote_ip = models.CharField(max_length=64)
     request_data = models.TextField()
     response_data = models.TextField()
-    request_time = models.IntegerField()
-    response_time = models.IntegerField()
+    taking = models.IntegerField()
+    create_time = models.IntegerField()
 
     class Meta:
         managed = False
